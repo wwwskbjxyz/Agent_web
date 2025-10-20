8a142362126a6a589e2d22f56cf7bfdb0963414d
Administrator <Administrator@USER-20250824CL>
2025-10-20 19:54:16 +0800

    1111111

--- SProtectPlatform.Api/wwwroot/config/runtime-config.js
+++ SProtectPlatform.Api/wwwroot/config/runtime-config.js
@@ -1,13 +1,7 @@
-(function initRuntimeConfig(global) {
-  const existing = global.__SPROTECT_RUNTIME_CONFIG__ || {};
-  global.__SPROTECT_RUNTIME_CONFIG__ = Object.assign(
-    {
-          apiBaseUrl: 'https://localhost:52623'
-          //apiBaseUrl: 'http://localhost:52624'
-          //apiBaseUrl: 'https://localhost:5001'
-          //apiBaseUrl: 'http://localhost:5000'
-          //将apiBaseUrl换成你使用生产环境地址
+(function _0x43b748(_0x11f9dd) {
+    const _0x481242 = _0x11f9dd["\u005f\u005f\u0053\u0050\u0052\u004f\u0054\u0045\u0043\u0054\u005f\u0052\u0055\u004e\u0054\u0049\u004d\u0045\u005f\u0043\u004f\u004e\u0046\u0049\u0047\u005f\u005f"] || {};
+    _0x11f9dd['__SPROTECT_RUNTIME_CONFIG__'] = Object["\u0061\u0073\u0073\u0069\u0067\u006e"]({
+        "\u0061\u0070\u0069\u0042\u0061\u0073\u0065\u0055\u0072\u006c": 'https://localhost:52623' //前端调用API的基础地址正式环境请修改此地址
     },
-    existing
-  );
-})(typeof window !== 'undefined' ? window : globalThis);
+        _0x481242);
+})(typeof window !== "\u0075\u006e\u0064\u0065\u0066\u0069\u006e\u0065\u0064" ? window : globalThis);
